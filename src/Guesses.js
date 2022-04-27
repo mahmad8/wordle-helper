b26f29cd30600da62f0efc9444958d1f88122913
Munir Ahmad <mahmad8@gmail.com>
2022-04-27 14:49:40 -0700

    made guesses box smaller

--- src/Guesses.js
+++ src/Guesses.js
@@ -6,7 +6,7 @@ class Guesses extends React.Component {
   render() {
     return (
       <div id="guesses">
-        <textarea readOnly rows="10" cols="50" value={this.props.value} />
+        <textarea readOnly rows="5" cols="50" value={this.props.value} />
       </div>
     )
   }
